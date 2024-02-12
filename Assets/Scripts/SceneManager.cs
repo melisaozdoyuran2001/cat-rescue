938e5a0f1b64a9a31145089a9570e344139d23fb
melisaozdoyuran <melisaozdoyuran@hotmail.com>
2024-02-12 14:26:17 -0600

    added instructions scene

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,6 +6,14 @@ using UnityEngine.SceneManagement;
 public class SceneManager : MonoBehaviour
 {
     public void StartGame() {
-         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+         UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void Instructions() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void OpenStartScene() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
     }
 }
