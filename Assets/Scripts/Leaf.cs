ce81d1f1fee1044f7d1399011a06e32045e6e53a
GrandOcelot <68270927+GrandOcelot@users.noreply.github.com>
2024-02-26 17:19:13 -0600

    Add files via upload

--- Assets/Scripts/Leaf.cs
+++ Assets/Scripts/Leaf.cs
@@ -7,11 +7,19 @@ public class Leaf : MonoBehaviour
 
     public CapsuleCollider2D capsuleCollider;
     public Rigidbody2D RB;
-    public float cooltime = 1.25f;
+    public static float cooltime = .5f;
+    public float cur_cooltime = cooltime;
     public int stage = 0;
     public bool PlayerOn = false;
     public GameObject character;
     private Rigidbody2D characterRigidbody;
+    public bool left_side_drops;
+    public SpriteRenderer sprite;
+    private Color default_green = new Color32(102, 176, 105, 255);
+    private Color player_green = new Color32(124, 173, 24, 255);
+    private Color yelllow_ = new Color32(235, 207, 24, 255);
+    private Color orange_ = new Color32(235, 122, 24, 255);
+    private Color red_ = new Color32(168, 49, 24, 255);
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +27,7 @@ public class Leaf : MonoBehaviour
         capsuleCollider = GetComponent<CapsuleCollider2D>();
         RB = GetComponent<Rigidbody2D>();
         characterRigidbody = character.GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,47 +35,94 @@ public class Leaf : MonoBehaviour
     {
         if (PlayerOn)
         {
-            cooltime -= Time.deltaTime;
-            if (cooltime <= 0)
+            cur_cooltime -= Time.deltaTime;
+            if (cur_cooltime <= 0)
             {
                 SetStage(stage + 1);
-                cooltime = 1.25f;
+                if (stage != 3)
+                {
+                    cur_cooltime = cooltime;
+                }
+                else
+                {
+                    cur_cooltime = cooltime * 2;
+                }
+
             }
         }
         else
         {
-            cooltime -= Time.deltaTime;
-            if (cooltime <= 0)
+            cur_cooltime -= Time.deltaTime;
+            if (cur_cooltime <= 0)
             {
                 SetStage(stage - 1);
-                cooltime = 1.25f;
+                cur_cooltime = cooltime;
             }
         }
     }
 
     void SetStage(int n)
     {
-        if(n == 0)
+        if (n == 0)
         {
             stage = n;
-            RB.rotation = 0;
+            if (left_side_drops)
+            {
+                RB.rotation = 0;
+            }
+            else
+            {
+                RB.rotation = 0;
+            }
+            if (PlayerOn)
+            {
+                sprite.color = player_green;
+            }
+            else
+            {
+                sprite.color = default_green;
+            }
         }
         else if (n == 1)
         {
             stage = n;
-            RB.rotation = 10;
+            if (left_side_drops)
+            {
+                RB.rotation = 10;
+            }
+            else
+            {
+                RB.rotation = -10;
+            }
+            sprite.color = yelllow_;
         }
         else if (n == 2)
         {
             stage = n;
-            RB.rotation = 30;
+            if (left_side_drops)
+            {
+                RB.rotation = 30;
+            }
+            else
+            {
+                RB.rotation = -30;
+            }
             capsuleCollider.enabled = true;
+            sprite.color = orange_;
         }
         else if (n == 3)
         {
             stage = n;
-            RB.rotation = 50;
+            if (left_side_drops)
+            {
+                RB.rotation = 50;
+            }
+            else
+            {
+                RB.rotation = -50;
+            }
             capsuleCollider.enabled = false;
+            sprite.color = red_;
         }
     }
 
@@ -75,6 +131,10 @@ public class Leaf : MonoBehaviour
         if (collision.gameObject.name == "Character")
         {
             PlayerOn = true;
+            if (stage == 0)
+            {
+                sprite.color = player_green;
+            }
         }
     }
 
@@ -83,9 +143,13 @@ public class Leaf : MonoBehaviour
         if (collision.gameObject.name == "Character")
         {
             PlayerOn = false;
-            cooltime = 1.25f;
+            cur_cooltime = cooltime;
             character.transform.SetParent(null);
             characterRigidbody.gravityScale = 1;
+            if (stage == 0)
+            {
+                sprite.color = default_green;
+            }
         }
     }
 
